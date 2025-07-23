e006b49a6289156eac7fedba79e6a170e826ccb5
PeerHarush <peerharush@gmail.com>
2025-07-23 12:57:31 +0300

    Remove unused imports from Places component

--- src/components/places.js
+++ src/components/places.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'; 
 
 function Places() {
